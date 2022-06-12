4461caf2f3b41ec28d1d809e1247fc3babf1b597
José Enrique López Obeso <jose_lopez-obeso@bio-rad.com>
2022-06-12 16:02:52 -0500

    move code to app

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
- const fs = require('fs');
+const multiply  = require('./helpers/multiply');
 
-const multiply = ( base ) => {
-    console.clear();
-    console.log(` =================================== `);
-    console.log(` ======== Tabla del ${base} ======== `);
-    console.log(` =================================== `);
-
-    let out = '';
-    for (let index = 0; index < 10; index++) {
-        out += `${base} * ${index} = ${base * index}\n`;     
-    }
-    console.log(out);
-    // fs.writeFile(`table-${base}.txt`,out,(err) => {
-        
-    //     if (err) throw err;
-    //     console.log(`The file table-${base}.txt has been saved!`);
-    // });
-    try {
-     fs.writeFileSync(`table-${base}.txt`,out);   
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-multiply(5)
+multiply();
---
+++ helpers/multiply.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const multiply = ( base = 5 ) => {
+    console.clear();
+    console.log(` =================================== `);
+    console.log(` ======== Tabla del ${base} ======== `);
+    console.log(` =================================== `);
+
+    let out = '';
+    for (let index = 0; index < 10; index++) {
+        out += `${base} * ${index} = ${base * index}\n`;     
+    }
+    console.log(out);
+    // fs.writeFile(`table-${base}.txt`,out,(err) => {
+        
+    //     if (err) throw err;
+    //     console.log(`The file table-${base}.txt has been saved!`);
+    // });
+    try {
+     fs.writeFileSync(`table-${base}.txt`,out);   
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = multiply
