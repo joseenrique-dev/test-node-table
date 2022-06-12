cf741ae50e34fd599742091760ffb5f70d4f8c61
José Enrique López Obeso <jose_lopez-obeso@bio-rad.com>
2022-06-12 15:42:35 -0500

    work with io

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
- 
-const multiply = ( base ) => {
+ const fs = require('fs');
 
+const multiply = ( base ) => {
+    console.clear();
+    console.log(` =================================== `);
+    console.log(` ======== Tabla del ${base} ======== `);
+    console.log(` =================================== `);
+
+    let out = '';
     for (let index = 0; index < 10; index++) {
-        console.log(`Multiply in base ${base} * ${index} = ${index * base}`);     
+        out += `${base} * ${index} = ${base * index}\n`;     
     }
+    console.log(out);
+    fs.writeFile(`table-${base}.txt`,out,(err) => {
+        
+        if (err) throw err;
+        console.log(`The file table-${base}.txt has been saved!`);
+    });
 }
 
 multiply(5)
