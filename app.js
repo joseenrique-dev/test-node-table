a67eb9f5b7e4258b18f0f775638b723ee1ab6876
José Enrique López Obeso <jose_lopez-obeso@bio-rad.com>
2022-06-12 14:51:28 -0500

    first excercise

---
+++ app.js
@@ -0,0 +1,9 @@
+ 
+const multiply = ( base ) => {
+
+    for (let index = 0; index < 10; index++) {
+        console.log(`Multiply in base ${base} * ${index} = ${index * base}`);     
+    }
+}
+
+multiply(5)
